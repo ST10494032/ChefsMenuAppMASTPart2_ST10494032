4b00366feadfa6c559031b14bad2099debdd15bb
ST10494032 <ST10494032@vcconnect.edu.za>
2025-10-20 18:53:18 +0200

    Changed colors to better match

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -4,14 +4,14 @@ import { View, Text, StyleSheet } from 'react-native';
 export default function SettingsScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Settings & Info</Text>
-      <Text style={styles.text}>This app uses a gold & silver theme and keeps data in-memory for this PoE. All adding of menu items is done on the Menu tab (Home).</Text>
+      <Text style={styles.title}>Settings & Info (For Jokes)</Text>
+      <Text style={styles.text}>This app uses a gold/Silver Style and keeps data in-memory. All adding of menu items is done on the Menu tab (HomePage) using the "+" icon in the bottom right of the screen.</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#0f0f0f', padding: 20, paddingTop: 60 },
+  container: { flex: 1, backgroundColor: '#000000ff', padding: 20, paddingTop: 60 },
   title: { color: '#d4af37', fontSize: 22, fontWeight: '700', marginBottom: 8 },
-  text: { color: '#cfcfcf' },
+  text: { color: '#ffffffff' },
 });
