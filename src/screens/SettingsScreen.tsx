378da75ba961809762e4187fe19c457ca3755b63
ST10494032 <ST10494032@vcconnect.edu.za>
2025-10-21 11:10:02 +0200

    Added to the settings page

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -5,7 +5,8 @@ export default function SettingsScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Settings & Info (For Jokes)</Text>
-      <Text style={styles.text}>This app uses a gold/Silver Style and keeps data in-memory. All adding of menu items is done on the Menu tab (HomePage) using the "+" icon in the bottom right of the screen.</Text>
+      <Text style={styles.text}>This app uses a Gold and Silver Style to bgring out Christoffels profesionalism. It also keeps the data in-memory. All adding of menu items is done only by Christoffel by the HomePage using the "+" icon in the bottom right of the screen.</Text>
+      <Text style={styles.text}>This was developed for Christoffel by yours truly. Christoffelis a well kn own classy gentlamen with good taste for food. Making anithing touching your pallet a blessing for the Gods.</Text>
     </View>
   );
 }
