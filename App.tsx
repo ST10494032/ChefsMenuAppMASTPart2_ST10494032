b0641a98284d8ebef916b46c098bf5f3a53abf0a
ST10494032 <ST10494032@vcconnect.edu.za>
2025-11-10 10:57:02 +0200

    Changed imports on App.tsx

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './src/navigation/TabNavigator';
 import { MenuProvider } from './src/context/MenuContext';
-import { StatusBar } from 'expo-status-bar'
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
