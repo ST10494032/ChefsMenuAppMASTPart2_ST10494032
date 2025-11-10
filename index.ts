c1a750905846659921e6fc712577340039c81d7c
ST10494032 <ST10494032@vcconnect.edu.za>
2025-11-10 10:54:50 +0200

    Moved and Updated Index.ts

--- index.ts
+++
@@ -1,7 +0,0 @@
-import { registerRootComponent } from 'expo';
-
-import App from './App';
-
-// It also ensures that whether you load the app in Expo Go or in a native build
-
-registerRootComponent(App);
